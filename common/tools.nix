a291b43dc2e9a475ce1a9c190cfdb0254714fc84
J0hnze <ponix@protonmail.com>
2025-09-09 15:25:38 +0100

    add more kali tools

--- common/tools.nix
+++ common/tools.nix
@@ -1,9 +1,19 @@
-{config, ...}:
+{config, pkgs, ...}:
 
 {
 environment.systemPackages = with pkgs; [
-  nmap
+  binwalk
+  bloodhound
   burpsuite
-  
+  hashcat
+  hashcat-utils
+  feroxbuster
+  gowitness
+  ghidra
+  netexec
+  nmap
+  openvpn
+  rustup
+  seclists
   ];
 }
